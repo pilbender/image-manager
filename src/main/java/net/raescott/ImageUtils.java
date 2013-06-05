b5e6e68b722ef703e9d422a0162a86ad13183fea
Richard Scott Smith <scott@thescene.com>
2013-06-05 16:32:09 -0700

    Added logging to ImageUtils to describe the headless exception issue.
    Renamed all variables to the lowercase class names because it is harder
    to follow the short, inconsistent naming.

--- src/main/java/net/raescott/ImageUtils.java
+++ src/main/java/net/raescott/ImageUtils.java
@@ -1,5 +1,7 @@
 package net.raescott;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.swing.*;
@@ -13,48 +15,61 @@ import java.awt.image.PixelGrabber;
  */
 @Service
 public class ImageUtils {
+	private static final Logger logger = LoggerFactory.getLogger(ImageUtils.class);
+
 	public BufferedImage toBufferedImage(Image image) {
 		if (image instanceof BufferedImage) {
 			return (BufferedImage) image;
 		}
 		image = new ImageIcon(image).getImage();
 		boolean hasAlpha = hasAlpha(image);
-		BufferedImage bimage = null;
-		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		BufferedImage bufferedImage = null;
+		GraphicsEnvironment graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		try {
 			int transparency = Transparency.OPAQUE;
 			if (hasAlpha) {
 				transparency = Transparency.BITMASK;
 			}
-			GraphicsDevice gs = ge.getDefaultScreenDevice();
-			GraphicsConfiguration gc = gs.getDefaultConfiguration();
-			bimage = gc.createCompatibleImage(image.getWidth(null), image.getHeight(null), transparency);
+			GraphicsDevice graphicsDevice = graphicsEnvironment.getDefaultScreenDevice();
+			GraphicsConfiguration graphicsConfiguration = graphicsDevice.getDefaultConfiguration();
+			bufferedImage = graphicsConfiguration.createCompatibleImage(image.getWidth(null), image.getHeight(null), transparency);
 		} catch (HeadlessException e) {
+			logger.error("Image processing error, perhaps you need to add: JAVA_OPTS=\"-Djava.awt.headless=true to your Tomcat installation's catalina.sh");
+			logger.error(e.getMessage());
 		}
-		if (bimage == null) {
+		if (bufferedImage == null) {
 			int type = BufferedImage.TYPE_INT_RGB;
 			if (hasAlpha) {
 				type = BufferedImage.TYPE_INT_ARGB;
 			}
-			bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), type);
+			bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), type);
 		}
-		Graphics g = bimage.getGraphics();
-		g.drawImage(image, 0, 0, null);
-		g.dispose();
-		return bimage;
+		Graphics graphics = bufferedImage.getGraphics();
+		graphics.drawImage(image, 0, 0, null);
+		graphics.dispose();
+		return bufferedImage;
 	}
 
+	/**
+	 * Check for opacity.  If there is no alpha, the opacity is one.  We do this by only
+	 * checking the upper left pixel.  If there is an alpha, we return true.  Otherwise
+	 * there is no alpha and we return false.
+	 *
+	 * @param image
+	 * @return
+	 */
 	public boolean hasAlpha(Image image) {
 		if (image instanceof BufferedImage) {
-			BufferedImage bimage = (BufferedImage) image;
-			return bimage.getColorModel().hasAlpha();
+			BufferedImage bufferedImage = (BufferedImage) image;
+			return bufferedImage.getColorModel().hasAlpha();
 		}
-		PixelGrabber pg = new PixelGrabber(image, 0, 0, 1, 1, false);
+		// Grab the upper left pixel of the image, we only need one.
+		PixelGrabber pixelGrabber = new PixelGrabber(image, 0, 0, 1, 1, false);
 		try {
-			pg.grabPixels();
+			pixelGrabber.grabPixels();
 		} catch (InterruptedException e) {
 		}
-		ColorModel cm = pg.getColorModel();
-		return cm.hasAlpha();
+		ColorModel colorModel = pixelGrabber.getColorModel();
+		return colorModel.hasAlpha();
 	}
 }
