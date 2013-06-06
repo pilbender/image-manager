639158aded3a25bd0219ba3e9e332f2d2ac047f9
Richard Scott Smith <scott@thescene.com>
2013-06-05 18:11:35 -0700

    Had a lot of trouble getting methods for manipulating file extensions to work so I wired up some JUnit tests.
    These fire up a Spring Bean container, inject configured beans based on
    testing spring configs instead of the normal application configs.
    
    Added the Spring Test Runner dependencies.

---
+++ src/test/java/net/raescott/ImageUtilsTest.java
@@ -0,0 +1,56 @@
+package net.raescott;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * @author Richard Scott Smith <scott.smith@isostech.com>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/test-context.xml"})
+public class ImageUtilsTest {
+
+	@Resource(name = "imageUtils")
+	ImageUtils imageUtils;
+
+	@Test
+	@Ignore
+	public void testToBufferedImage() throws Exception {
+
+	}
+
+	@Test
+	@Ignore
+	public void testHasAlpha() throws Exception {
+
+	}
+
+	@Test
+	public void testGetFilenameExtension() throws Exception {
+		System.out.println("getFilenameExtension");
+
+		String result = imageUtils.getFilenameExtension("test.jpg");
+		assertEquals("jpg", result);
+
+		result = imageUtils.getFilenameExtension("test");
+		assertEquals("", result);
+	}
+
+	@Test
+	public void testGetFilenameWithoutExtension() throws Exception {
+		System.out.println("getFilenameWithoutExtension");
+
+		String result = imageUtils.getFilenameWithoutExtension("test.jpg");
+		assertEquals("test", result);
+
+		result = imageUtils.getFilenameWithoutExtension("test");
+		assertEquals("test", result);
+	}
+}
